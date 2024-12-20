3b1cfd2e73ca5dbd5dda8e99caab19af2f542ef8
iam-ehsan <ehsanullah@wanclouds.net>
2024-12-20 16:21:34 +0500

    added new routes

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ import logging
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    logging.warning("hello world log message")
-    return 'Hello World'
+def warning():
+    logging.warning("Warning message")
+    return 'Warning message'
+
+@app.route('/info')
+def info():
+    logging.info("Info log message")
+    return 'Info log message'
+
+@app.route('/error')
+def error():
+    logging.error("Error log message")
+    return 'Error !!!!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
