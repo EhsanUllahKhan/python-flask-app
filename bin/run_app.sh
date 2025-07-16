47ac138f4197837aba4aa72362fc91c065920a3d
Ehsan Ullah Khan <72066636+EhsanUllahKhan@users.noreply.github.com>
2025-07-16 18:45:38 +0500

    Update run_app.sh

--- bin/run_app.sh
+++ bin/run_app.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true \
-OTEL_RESOURCE_ATTRIBUTES='service.name=test-flask' \
-OTEL_EXPORTER_OTLP_ENDPOINT='ingest.us.signoz.cloud:443' \
-OTEL_EXPORTER_OTLP_HEADERS=signoz-ingestion-key=e8cfe77c-b82a-4022-91fe-b81eb2b48700 \
-opentelemetry-instrument --traces_exporter otlp --metrics_exporter otlp --logs_exporter otlp python main.py
+python main.py
